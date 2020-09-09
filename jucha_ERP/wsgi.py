452ef83314e91e98c71882cae1d4968787edac45
GogoHYX <38928580+GogoHYX@users.noreply.github.com>
2020-09-09 17:31:08 +0800

    1

--- jucha_ERP/wsgi.py
+++ jucha_ERP/wsgi.py
@@ -19,6 +19,6 @@ PROJECT_DIR = dirname(dirname(abspath(__file__)))
 sys.path.insert(0, PROJECT_DIR)
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travel_record.settings")
-os.environ["DJANGO_SETTINGS_MODULE"] = "travel_record.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "jucha_ERP.settings"
 
 application = get_wsgi_application()
