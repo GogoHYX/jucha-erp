f35ff9b04f35f632a4a455cbaf429cacc3ef07a6
GogoHYX <38928580+GogoHYX@users.noreply.github.com>
2020-09-10 11:06:59 +0800

    1

--- jucha_ERP/wsgi.py
+++ jucha_ERP/wsgi.py
@@ -9,21 +9,17 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 import sys
-import site
 from os.path import dirname, abspath
-
+from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jucha_ERP.settings')
-python_home = '/home/jucha/django'
-python_version = '.'.join(map(str, sys.version_info[:2]))
-site_packages = python_home + '/lib/python%s/site-packages' % python_version
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
 sys.path.insert(0, PROJECT_DIR)
-site.addsitedir(site_packages)
+
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travel_record.settings")
 os.environ["DJANGO_SETTINGS_MODULE"] = "jucha_ERP.settings"
 print(sys.path)
 print(sys.version)
 
-from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
